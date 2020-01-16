2f40889bc2f70c8ea1ae2f2e8c9cd350d74d5429
Bruno Schmidt <bruno.schmidt@gmail.com>
2020-01-15 21:02:22 -0300

    Comentarios e optimizacao

--- splice.js
+++ splice.js
@@ -1,15 +1,19 @@
 function splice(arr, begin, count, items) {
     if (begin === undefined) {
         begin = 0;
+    } else if (begin < 0) {
+        begin = arr.length + begin;
     }
     if (count === undefined) {
         count = 0;
     }
 
+    // separa os pedaços
     let start = arr.slice(0, begin);
     let kept = arr.slice(begin, begin + count);
     let end = arr.slice(begin + count);
 
+    // junta os pedaços necessários
     let result;
     if (items !== undefined) {
         result = start.concat(items).concat(end);
@@ -17,21 +21,21 @@ function splice(arr, begin, count, items) {
         result = start.concat(end);
     }
 
+    // move para o array original
     for (let index = 0; index < result.length; index++) {
         arr[index] = result[index];
     }
-    let countClear = arr.length - result.length;
-    for (let index = 0; index < countClear; index++) {
-        arr.pop();
-    }
+    // remove os itens excedentes
+    arr.length = result.length;
 
+    // retorna o que foi substituido
     return kept;
 }
 
 let array = [1, 2, 3];
 console.log(array);
 
-let kept = splice(array, 1, 3, 'a');
+let kept = splice(array, -2, 1, 'a');
 
 console.log(array);
 console.log(kept);
